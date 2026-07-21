594a3bfc863e47846b625535eefe52004297cfbe
Jimmy Kuang <jimmy.kuang96@gmail.com>
2026-07-21 16:04:43 -0700

    Adds tabledef struct. Moves table variable creation after the existence check in CreateTable

--- data/database.go
+++ data/database.go
@@ -3,12 +3,12 @@ package data
 import "errors"
 
 func (db *Database) CreateTable(name string) error {
-	t := &Table{
-		Name: name,
-	}
-
 	if _, exists := db.Tables[name]; exists {
 		return errors.New("table already exists")
+	}
+
+	t := &Table{
+		Name: name,
 	}
 
 	db.Tables[t.Name] = t
--- repl/types.go
+++ repl/types.go
@@ -1,6 +1,13 @@
 package repl
 
+import "rdbslite/data"
+
 type Command struct {
 	Name string
 	Args []string
+}
+
+type TableDef struct {
+	Name   string
+	Schema []data.Column
 }

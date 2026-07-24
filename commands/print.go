fcd8e9ca089790a45a9e1e3549541dd416fa25de
Jimmy Kuang <66882632+JimmyKuangg@users.noreply.github.com>
2026-07-23 21:20:28 -0700

    Adds row printing (#6)

--- commands/print.go
+++ commands/print.go
@@ -18,35 +18,80 @@ func Print(db *data.Database, cmd Command) (string, error) {
 }
 
 func renderTable(table *data.Table) string {
-	nameWidth := len("Name")
-	typeWidth := len("Type")
-
-	for _, col := range table.Schema {
-		if len(col.Name) > nameWidth {
-			nameWidth = len(col.Name)
-		}
-		typeText := string(col.Type)
-		if len(typeText) > typeWidth {
-			typeWidth = len(typeText)
-		}
-	}
-
-	border := "+" + strings.Repeat("-", nameWidth+2) + "+" + strings.Repeat("-", typeWidth+2) + "+"
+	headers := buildHeaders(table)
+	widths := buildWidths(headers, table.Rows)
+	border := buildBorder(widths)
 
 	var out strings.Builder
 	out.WriteString("TABLE ")
 	out.WriteString(table.Name)
 	out.WriteString("\n")
+
 	out.WriteString(border)
 	out.WriteString("\n")
-	out.WriteString(fmt.Sprintf("| %-*s | %-*s |\n", nameWidth, "Name", typeWidth, "Type"))
+	writeRow(&out, headers, widths)
 	out.WriteString(border)
 	out.WriteString("\n")
 
-	for _, col := range table.Schema {
-		out.WriteString(fmt.Sprintf("| %-*s | %-*s |\n", nameWidth, col.Name, typeWidth, col.Type))
+	for _, r := range table.Rows {
+		cells := make([]string, len(headers))
+		for i := 0; i < len(headers); i++ {
+			if i < len(r.Values) {
+				cells[i] = fmt.Sprint(r.Values[i])
+			} else {
+				cells[i] = ""
+			}
+		}
+		writeRow(&out, cells, widths)
 	}
 
 	out.WriteString(border)
 	return out.String()
+}
+
+func buildHeaders(table *data.Table) []string {
+	headers := make([]string, len(table.Schema))
+	for i, col := range table.Schema {
+		headers[i] = col.Name
+	}
+	return headers
+}
+
+func buildBorder(widths []int) string {
+	var b strings.Builder
+	b.WriteString("+")
+	for _, w := range widths {
+		b.WriteString(strings.Repeat("-", w+2))
+		b.WriteString("+")
+	}
+	return b.String()
+}
+
+func buildWidths(headers []string, rows []data.Row) []int {
+	widths := make([]int, len(headers))
+	for i, h := range headers {
+		widths[i] = len(h)
+	}
+
+	for _, r := range rows {
+		for i := 0; i < len(headers) && i < len(r.Values); i++ {
+			cell := fmt.Sprint(r.Values[i])
+			if len(cell) > widths[i] {
+				widths[i] = len(cell)
+			}
+		}
+	}
+	return widths
+}
+
+func writeRow(out *strings.Builder, cells []string, widths []int) {
+	out.WriteString("|")
+	for i, w := range widths {
+		cell := ""
+		if i < len(cells) {
+			cell = cells[i]
+		}
+		out.WriteString(fmt.Sprintf(" %-*s |", w, cell))
+	}
+	out.WriteString("\n")
 }
